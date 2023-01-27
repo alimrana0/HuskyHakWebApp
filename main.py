fe545f7e412d9dc26090f58d06920e74daee2170
Ali Rana <82501413+alimrana0@users.noreply.github.com>
2023-01-26 20:10:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.secret_key = 'your secret key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Basketball7867!'
+app.config['MYSQL_PASSWORD'] = '*********'
 app.config['MYSQL_DB'] = 'husky_app'
 
 mysql = MySQL(app)
